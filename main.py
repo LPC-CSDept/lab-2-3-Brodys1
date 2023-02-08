ce7fe9e3c3b75def940d2a83c1363f2a04252663
Brodys1 <123720584+Brodys1@users.noreply.github.com>
2023-02-07 17:22:39 -0800

    lab3

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     ##################################################
     # Comlete your code here
+    val1 = int(input('Value one : '))
+    val2 = int(input('Value two : '))
+    val3 = int(input('Value three : '))
+    sum = val1 + val2 + val3; avg = sum/3
+    print (f'\nValues:',val1, val2, val3)
+    print(f'Total: \t\t{sum}\nAverage: \t{avg:.2f}')
     ##################################################
 
     pass
