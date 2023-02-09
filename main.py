4a5ac8c865bae90485130ac4f9a0bc8011c0cfec
Kyuwoong Lee <kwooler@gmail.com>
2023-02-08 23:13:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     val2 = int(input('Value two : '))
     val3 = int(input('Value three : '))
     sum = val1 + val2 + val3; avg = sum/3
-    print (f'\nValues:',val1, val2, val3)
+    print (f'Values:',val1, val2, val3)
     print(f'Total: \t\t{sum}\nAverage: \t{avg:.2f}')
     ##################################################
 
